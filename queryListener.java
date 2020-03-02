25965913ea58e52cf2793215d98c7f7b7c3efdda
nathanmandell <nathanmandell@tamu.edu>
2020-03-01 19:47:43 -0600

    Action listener for GO button implemented

---
+++ queryListener.java
@@ -0,0 +1,42 @@
+/*
+Nathan Mandell
+CSCE 315
+*/
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class queryListener implements ActionListener{
+  JComboBox queryListDdn;
+  JButton runQueryBtn;
+  String options[] = {"Largest Football Stadiums", "10 Most Attended Games", "Mystery Query"};
+  JFrame mainWindow;
+  public queryListener(JFrame main){
+    mainWindow = main;
+    queryListDdn = new JComboBox(options);
+    runQueryBtn = new JButton("GO!");
+
+    runQueryBtn.setBackground(new Color(133,154,188));
+    runQueryBtn.setOpaque(true);
+    runQueryBtn.setForeground(Color.WHITE);
+    queryListDdn.addActionListener(this);
+    runQueryBtn.addActionListener(this);
+
+    mainWindow.add(queryListDdn, BorderLayout.CENTER);
+    mainWindow.add(runQueryBtn, BorderLayout.EAST);
+  }
+
+  public void actionPerformed(ActionEvent e){
+    if(e.getActionCommand() == "GO!"){
+
+      System.out.println(jdbcpostgreSQLGUI.executeQuery((String)queryListDdn.getSelectedItem()));
+    }
+    /*
+    System.out.println(e.getActionCommand());
+    System.out.println(e.getModifiers());
+    System.out.println(e.paramString());
+    */
+  }
+
+}
