28dbc4ae2fdf515f7dfbfef7b31fbd253fb26811
nathanmandell <nathanmandell@tamu.edu>
2020-03-01 19:44:50 -0600

    executeQuery function added

--- jdbcpostgreSQLGUI.java
+++ jdbcpostgreSQLGUI.java
@@ -29,7 +29,9 @@ Robert lightfoot
 CSCE 315
 9-25-2019
  */
-public class jdbcpostgreSQLGUI implements ActionListener {
+public class jdbcpostgreSQLGUI {
+  static Connection conn;
+  
   public static void main(String args[]) {
     //http://sandsduchon.org/duchon/Musings/a/macButton.html
     try {
@@ -53,7 +55,7 @@ public class jdbcpostgreSQLGUI implements ActionListener {
     
 
     //Building the connection
-     Connection conn = null;
+     conn = null;
      try
      {
         Class.forName("org.postgresql.Driver");
@@ -66,22 +68,10 @@ public class jdbcpostgreSQLGUI implements ActionListener {
         System.err.println(e.getClass().getName()+": "+e.getMessage());
         System.exit(0);
      }
+ 
+    String options[] = {"Largest Football Stadiums", "10 Most Attended Games", "Mystery Query"};
     
-    String[] options = new String[] {"Largest Football Stadiums", "10 Most Attended Games", "Mystery Query"};
-
-    //https://docs.oracle.com/javase/tutorial/uiswing/components/combobox.html
-    JComboBox querylist = new JComboBox(options);
-    querylist.addActionListener(querylist);
-
-    mainWindow.add(querylist, BorderLayout.CENTER);
-
-    JButton activateQuery = new JButton("GO!");
-    activateQuery.setBackground(new Color(133,154,188));
-    activateQuery.setOpaque(true);
-    //activateQuery.setBorderPainted(false);
-    activateQuery.setForeground(Color.WHITE);
-    mainWindow.add(activateQuery, BorderLayout.EAST);
-
+    queryListener qL = new queryListener(mainWindow);
     int response = JOptionPane.showOptionDialog(null,"Select what you want to see?",null,JOptionPane.YES_NO_OPTION,JOptionPane.PLAIN_MESSAGE,null,options,options[0]);
 
      String name = "";
@@ -134,6 +124,7 @@ public class jdbcpostgreSQLGUI implements ActionListener {
   mainWindow.add(img, BorderLayout.SOUTH);
   mainWindow.setVisible(true);
 
+  /*
   //closing the connection
     try {
       conn.close();
@@ -141,21 +132,41 @@ public class jdbcpostgreSQLGUI implements ActionListener {
     } catch(Exception e) {
       JOptionPane.showMessageDialog(null,"Connection NOT Closed.");
     }//end try catch
+  */
   }//end main
 
-  public void actionPerformed(ActionEvent e) {
-    JComboBox cb = (JComboBox)e.getSource();
-    String queryChoice = (String)cb.getSelectedItem();
-    switch (queryChoice) {
-      case "Largest Football Stadiums":
-        System.out.println("Howdy");
-        break;
-      case "Top 10 Most Attended Games":
-        System.out.println("Test");
-        break;
-      case "Mystery Query":
-        System.out.println("Hello");
-        break;
+  public static String executeQuery(String query){
+    String name = "";
+    try{
+      Statement stmt = conn.createStatement();
+      
+      if(query.equals("Largest Football Stadiums")){
+        String sqlStatement="SELECT * FROM merged_stadium ORDER BY \"Capacity\" DESC LIMIT 10";
+      
+        ResultSet result = stmt.executeQuery(sqlStatement);
+        
+	//JOptionPane.showMessageDialog(null,"Lets look at the top 10 Largest Football Stadiums in the World! .");
+        name += "Stadium Code\t Name\t Capcacity\n\n";
+
+        while (result.next()) {
+            name+= result.getString("Stadium Code") + " " + result.getString("Name") + " "+ result.getString("Capacity") + "\n";
+        }
+      }
+      else if(query.equals("10 Most Attended Games")){
+        String sqlStatement =  "SELECT stats.\"Attendance\", game.\"Date\", merged_stadium.\"Name\" FROM merged_game_statistics AS stats JOIN merged_game AS game ON stats.\"Game Code\"=game.\"Game Code\" JOIN merged_stadium ON game.\"Stadium Code\"=merged_stadium.\"Stadium Code\" ORDER BY stats.\"Attendance\" DESC LIMIT 10";
+        // String sqlStatement = "SELECT COUNT(*) FROM merged_game JOIN merged_stadium ON merged_game.\"Stadium Code\"= merged_stadium.\"Stadium Code\" WHERE \"Name\" = "+ stadium_name+"";
+        ResultSet result = stmt.executeQuery(sqlStatement);
+        while (result.next()) {
+            name+= result.getString("Attendance") + " " + result.getString("Date") + " "+ result.getString("Name") + "\n";
+        }
+      }
     }
+   
+    catch (Exception e){
+      //JOptionPane.showMessageDialog(null,"Error accessing Database.");
+      return "Error accessing Database.";
+    }
+    return name;
   }
+
 }//end Class
